b26c2181b6be70bb86228f1a24c7a520000cce8a
marcinoles87 <marcinoles87@gmail.com>
2023-09-03 10:33:34 +0200

    add title components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
-     
+     <Title></Title>
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div className='title-container'>
+        <h1>Title Gallery</h1>
+        <h2>title picture</h2>
+        <p>Your Lorem ipsum personal webApp for gallery</p>
+    </div>
+  )
+}
+
+export default  Title

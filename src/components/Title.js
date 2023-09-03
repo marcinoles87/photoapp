00fd2e8d233abe9e60ffb5f9aad5d2d5cbb668bc
marcinoles87 <marcinoles87@gmail.com>
2023-09-03 10:36:54 +0200

    add title components

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './title.css'
 
 function Title() {
   return (
     <div className='title-container'>
         <h1>Title Gallery</h1>
-        <h2>title picture</h2>
+        <h2>Your Picture</h2>
         <p>Your Lorem ipsum personal webApp for gallery</p>
     </div>
   )

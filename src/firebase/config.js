684dcc7d74d4281b1656ffe077dee0028b920b4e
marcinoles87 <marcinoles87@gmail.com>
2023-09-04 05:30:43 +0200

    add firebase to project

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWL4RRhlbEkQXAlk_f0RsVdsE-RbUGBp4",
+  authDomain: "marcinoles87-photoapp.firebaseapp.com",
+  projectId: "marcinoles87-photoapp",
+  storageBucket: "marcinoles87-photoapp.appspot.com",
+  messagingSenderId: "790173186276",
+  appId: "1:790173186276:web:5cd31552394dcc293954f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

bf9b29cc79b4886aa9626fa2e5dc81f01b8a9ec1
marcinoles87 <marcinoles87@gmail.com>
2023-09-04 05:39:26 +0200

    add firebase to project

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import 'firebase/storage'
+import 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +17,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const projectStorage = app.storage();
+const projectFirestore = app.firestore();
+
+export { projectStorage , projectFirestore}

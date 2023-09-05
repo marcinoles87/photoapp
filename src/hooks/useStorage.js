94338d60ca59a4ea210de71704cac6378516dcf8
marcinoles87 <marcinoles87@gmail.com>
2023-09-05 15:26:26 +0200

    upload form

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -2,7 +2,22 @@ import React from 'react'
 import { useState , useEffect } from 'react';
 import {projectStorage} from '../firebase/config'
 
-function useStorage() {
+function useStorage(file) {
+    const [progress , setProgress] = useState(0);
+    const [error , setError] = useState(null);
+    const [url , setUrl] = useState(null);
+
+    useEffect( () => {
+        //references
+        const storageRef = projectStorage.ref(file.name);
+        storageRef.put(file).on('state changed' , (snap) => {
+            
+        })
+
+    } , [file] )
+
+
+
   return (
     <div>useStorage</div>
   )

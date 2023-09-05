ffc45652db308f75a420f2956430023c83844ad8
marcinoles87 <marcinoles87@gmail.com>
2023-09-05 15:20:44 +0200

    upload form

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState , useEffect } from 'react';
+import {projectStorage} from '../firebase/config'
+
+function useStorage() {
+  return (
+    <div>useStorage</div>
+  )
+}
+
+export default useStorage

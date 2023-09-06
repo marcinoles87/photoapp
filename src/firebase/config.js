770e76ffc04695216bf526e8e048ce4a370e42bd
marcinoles87 <marcinoles87@gmail.com>
2023-09-06 15:14:03 +0200

    upload form

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import * as firebase from 'firebase/app';
-import 'firebase/storage'
-import 'firebase/firestore'
+
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,8 +22,8 @@ const app = initializeApp(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig)
 
-const projectStorage = firebase.storage();
-const projectFirestore = firebase.firestore();
+const projectStorage = getStorage();
+const projectFirestore = getFirestore();
 
 
 

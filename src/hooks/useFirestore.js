6b75072d474c6a686630b2c544b977588ebce622
marcinoles87 <marcinoles87@gmail.com>
2023-09-12 20:35:35 +0200

    work with collection

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,19 +1,18 @@
 import { useEffect , useState } from "react";
 import { projectFirestore } from "../firebase/config";
-import { collection } from "firebase/firestore";
+import { collection, onSnapshot , doc, QuerySnapshot} from "firebase/firestore";
 
 const useFirestore = (coll) => {
   const [docs , setDoc] = useState([]);
   console.log(projectFirestore)
 
   useEffect( () => {
-    const unsub = collection(projectFirestore,collection)
-    .onSnapShot( (snap) => {
-      let documents = [];
-      snap.forEach(doc => {
-        documents.push({...doc.data() , id : doc.id})
-      })
-      setDoc(documents);
+    const unsub = onSnapshot(doc(projectFirestore , 'images' , 'sf') , (docum) =>
+    {
+     const documents = [];
+     docum.forEach( (doc) => {
+      documents.push( {...doc.data(), id:doc.id})
+     })
     })
 
     return () => unsub();

f28d1575f3a8728579fb8a55840299f4552250cd
marcinoles87 <marcinoles87@gmail.com>
2023-09-13 05:47:42 +0200

    work with collection

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -3,7 +3,7 @@ import useFirestore from '../hooks/useFirestore'
 
 function ImageGrid() {
   const {docs} = useFirestore('images')
-  console.log(docs)
+  
   return (
     <div className='img-grid'>ImageGrid</div>
   )
--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -4,23 +4,19 @@ import { collection, onSnapshot , doc, QuerySnapshot} from "firebase/firestore";
 
 const useFirestore = (coll) => {
   const [docs , setDoc] = useState([]);
-  console.log(projectFirestore)
+  
 
-  useEffect( () => {
-    const unsub = onSnapshot(doc(projectFirestore , 'images' , 'sf') , (docum) =>
+  useEffect( (coll) => {
+    const unsub = onSnapshot(doc(projectFirestore , "images" , 'img') , (snap) =>
     {
-     const documents = [];
-     documents.forEach( (doc) => {
-      documents.push( {...doc.data(), id:doc.id})
-     })
-
-     console.log(documents)
-    })
-
-    return () => unsub();
+      console.log(snap.data())
   } , [coll])
 
-  return {docs}
-}
+
+  setDoc(null)
+})
+
+return {docs}
+ }
 
 export default useFirestore

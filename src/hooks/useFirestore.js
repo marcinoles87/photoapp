2c62badb8a3d8dd70db686d6e131299b90d40a2b
marcinoles87 <marcinoles87@gmail.com>
2023-09-15 05:33:15 +0200

    work with collection

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,6 +1,6 @@
 import { useEffect , useState } from "react";
 import { projectFirestore } from "../firebase/config";
-import { collection,getDocs, onSnapshot , docs, QuerySnapshot, Firestore, getFirestore} from "firebase/firestore";
+import { collection,getDocs, onSnapshot , doc, QuerySnapshot, Firestore, getFirestore} from "firebase/firestore";
 
 const useFirestore = (coll) => {
   const [docs , setDoc] = useState([]);
@@ -8,14 +8,13 @@ const useFirestore = (coll) => {
 
   useEffect( () => {
     const unsub = collection(projectFirestore , 'images')
-    // const docRef = docs(projectFirestore , 'images' )
-    const docSnap = getDocs(unsub , 'images')
-   
+    const docRef = doc(unsub , 'images' )
+    const docSnap = getDocs(unsub , 'images')  
     console.log(unsub)
+    console.log(docRef)
     console.log(docSnap)
-    // console.log(docSnap)
     
-
+    console.log(docSnap.data)
     
   } , [])
     

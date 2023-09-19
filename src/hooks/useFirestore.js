772bd61eb2fa314c36fefad4d556d3f8961be9ca
marcinoles87 <marcinoles87@gmail.com>
2023-09-19 05:42:19 +0200

    work with code

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,6 +1,6 @@
 import { useEffect , useState } from "react";
 import { projectFirestore } from "../firebase/config";
-import { collection,getDocs, onSnapshot , doc, QuerySnapshot, Firestore, getFirestore, getDocFromCache, query, where , CollectionReference} from "firebase/firestore";
+import { collection,getDocs, onSnapshot , doc, QuerySnapshot, Firestore , CollectionReference} from "firebase/firestore";
 
 function useFirestore  (coll)  {
   const [docs , setDoc] = useState([]);
@@ -21,23 +21,7 @@ const unsub = collection(projectFirestore , 'images')
       
     }
     
-    // const docRef = doc(unsub , 'images' )
-    // const docSnap = getDocs(unsub , 'images') 
-    // const q = query(unsub , where ('images' , "==" , true)) 
-    // const querySnapshot = getDocs(q);
   
-    // console.log(q)
-    // console.log(unsub)
-    // console.log(docRef)
-    // console.log(docSnap)
-
-    // const  documents = [];
-    
-    // documents.push('sss' , 'dsdf')
-    // console.log(documents)
-    // setDoc(
-      
-    // )
 
     getDocList();
   
@@ -45,19 +29,7 @@ const unsub = collection(projectFirestore , 'images')
     
     console.log(docs)
     return { docs }
-    
-
-    // const docRef = doc(db , 'images' , 'img')
-    // const docSnap = getDocs(imgCol);
-    // const newData = docSnap.map( item => item.data())
-    
-    
   
-
-
-  
-
-return {docs}
 
   }
  

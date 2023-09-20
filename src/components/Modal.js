99fc45bac2e75294d655eda865d71f377fdfa4f0
marcinoles87 <marcinoles87@gmail.com>
2023-09-20 15:40:31 +0200

    add motion framer to project

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,8 +8,8 @@ function Modal( {selected , setSelected}) {
   }
   return (
     <motion.div   
-    whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.9 }}
+    initial= {{scale:0}}
+    whileTap={{ scale: 0.9 , rotate : 180 }}
     className='backdrop'>
         <img className='enlarge-drop' src={selected} alt='enlarge pic' onClick={setClose}></img>
         <button className='backdrop-button' onClick={setClose}>X</button>

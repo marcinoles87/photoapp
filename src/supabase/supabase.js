5c0f8bd246df647b0955c4219f1294ecf960b0da
marcinoles87 <marcinoles87@gmail.com>
2026-04-27 16:39:49 +0200

    add new section

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://sttdevzmdstzhlralfyn.supabase.co'
-const supabaseKey = 'sb_publishable_pABpURHMdGrZkneaQujZFQ_d3zDzvxt'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = 'process.env.REACT_APP_SUPABASE_PUBLISHABLE_DEFAULT_KEY'
+
+// /'https://sttdevzmdstzhlralfyn.supabase.co'/
+// 'sb_publishable_pABpURHMdGrZkneaQujZFQ_d3zDzvxt' 
 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
